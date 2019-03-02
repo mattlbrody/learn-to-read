d0e825f4b68c56bcced73df059536ab1f9102300
Matthew Brody <matt@Matthews-MacBook-Air-3.local>
2019-03-02 16:28:23 +1000

    added jsx for more than 1 letter on the screen

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import ChooseTheLetter from './ChooseTheLetter';
+//import ChooseTheLetter from './ChooseTheLetter';
 import SoundReading from './SoundReading';
 
 class App extends Component {
@@ -7,7 +7,7 @@ class App extends Component {
 	render() {
 		return (
 			<div>
-				<ChooseTheLetter />
+				<SoundReading />
 			</div>
 		);
 	}
--- src/components/SoundReading.js
+++ src/components/SoundReading.js
@@ -1,43 +1,29 @@
 import './css/SoundReading.css';
 import React, { Component } from 'react';
 import SoundData from './sounds.json';
-import Activities from './activities.json';
 
 	class SoundReading extends Component {
 		state = { 
-			activity: Activities.activities[0],
-			sound: SoundData.sounds[0].letter, 
-			audio: SoundData.sounds[0].audio, 
-			index: 0, 
-			arrLength: SoundData.sounds.length 
+			data: SoundData.sounds,
+			index: 0
 		}
 
-		onHoverOver = () => {
-		let audioSound = new Audio(this.state.audio);
+		onHoverOver = (audio) => {
+		let audioSound = new Audio(audio);
 		return audioSound.play();
 		}
 
 		nextSound = () => {
-			if (this.state.arrLength > 1) {
-			this.setState({ 
-				activity: Activities.activities[this.state.index + 1],
-				sound: SoundData.sounds[this.state.index + 1].letter, 
-				audio: SoundData.sounds[this.state.index + 1].audio, 
-				index: this.state.index + 1, 
-				arrLength: this.state.arrLength - 1 
-			})
-			} else {
-				this.setState({ sound: 'Done!', audio: null, index: 0 })
-			}
-		}
+			this.setState({ index: (this.state.index + 1) });
+		};
 
 
 		render() {
-			console.log(this.state.activity.length)
-			if(this.state.activity.length === 2) {
+			if(Object.keys(this.state.data[this.state.index]).length === 4) {
 				return (
 					<div className="ui raised very padded text container segment twoletters">
-						<h1 className="soundLettersOut">{this.state.activity}</h1>
+						<h1 className="soundLettersOut">{this.state.data[this.state.index].letter0}</h1>
+						<h1 className="soundLettersOut">{this.state.data[this.state.index].letter1}</h1>
 						<div className="ui seven column grid">
 							<div className="leftcircle"></div>
 							<div className="left"></div>
@@ -52,22 +38,27 @@ import Activities from './activities.json';
 							 	<source src="horse.mp3" type="audio/mpeg" />
 								</audio>
 							</div>
+							<div onMouseEnter={this.onHoverOver} className="middlecircle">
+								<audio id="myAudio">
+							 	<source src="horse.mp3" type="audio/mpeg" />
+								</audio>
+							</div>
 							<div className="right"></div>
 							<div className="arrow-right"></div>
 						</div>
 						<button onClick={this.nextSound} className="ui primary button">Next Sound</button>
 					</div>
 				);
-			} else if (this.state.activity.length === 1){
+			} else if (Object.keys(this.state.data[this.state.index]).length === 2){
 				return (
 					<div className="ui raised very padded text container segment oneletter">
-						<h1>{this.state.activity}</h1>
+						<h1 className="soundLettersOut">{this.state.data[this.state.index].letter0}</h1>
 						<div className="ui five column grid">
 							<div className="leftcircle"></div>
 							<div className="left"></div>
-							<div onMouseEnter={this.onHoverOver} className="middlecircle">
+							<div onMouseEnter={this.onHoverOver(this.state.data[this.state.index].audio0)} className="middlecircle">
 								<audio id="myAudio">
-							 	<source src="horse.mp3" type="audio/mpeg" />
+							 	<source src={this.state.data[this.state.index].audio0} type="audio/mpeg" />
 								</audio>
 							</div>
 							<div className="right"></div>
